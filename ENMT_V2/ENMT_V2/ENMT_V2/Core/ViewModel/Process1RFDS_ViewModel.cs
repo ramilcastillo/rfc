332e2ead23603f23c96fb600e92a1f23d6287989
Ramil Castillo <ramil.castillo@rocketmail.com>
2018-06-04 03:49:34 +0800

    Delete Files Master

--- ENMT_V2/ENMT_V2/ENMT_V2/Core/ViewModel/Process1RFDS_ViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ENMT_V2.Core.ViewModel
-{
-    public class Process1RFDS_ViewModel
-    {
-        public string RFDS_ID { get; set; }
-    }
-}
--- ENMT_V2/ENMT_V2/ENMT_V2/Repository/Context.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Configuration;
-
-namespace ENMT_V2.Repository
-{
-    public class Context
-    {
-        public static string ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-    }
-}
